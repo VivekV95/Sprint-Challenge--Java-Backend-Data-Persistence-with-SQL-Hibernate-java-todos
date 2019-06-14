e34cd60e04aa55d64d2a3a0b21b789f69fc35eb0
VivekV95 <vivekv210@gmail.com>
2019-06-14 12:52:37 -0500

    Enabled custom handler error messages

--- src/main/kotlin/com/vivekvishwanath/todos/TodosApplication.kt
+++ src/main/kotlin/com/vivekvishwanath/todos/TodosApplication.kt
@@ -3,6 +3,7 @@ package com.vivekvishwanath.todos
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.web.servlet.DispatcherServlet
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @EnableWebMvc
@@ -11,5 +12,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc
 class TodosApplication
 
 fun main(args: Array<String>) {
-    runApplication<TodosApplication>(*args)
+    val ctx = runApplication<TodosApplication>(*args)
+
+    val dispatcherServlet: DispatcherServlet = ctx.getBean("dispatcherServlet") as DispatcherServlet
+    dispatcherServlet.setThrowExceptionIfNoHandlerFound(true)
+
 }
